72ad6bedcaf363953e6bc7a4e2473d4a0d9a7ced
OPQAM <theopqam@protonmail.com>
2025-09-15 07:50:56 +0100

    smart trick for calculating the string length

---
+++ Assembly/NASM/ASMtutor/02_calculate_string_length.asm
@@ -0,0 +1,35 @@
+; Hello program, with string length calculation
+; Compile with: nasm -f elf 02_calculate_string_length.asm
+; Link with: ld -m elf_i386 02_calculate_string_length.o -o 02_calculate
+
+SECTION .data
+msg     db      'Hello darkness my old friend...', 0Ah
+
+SECTION .text
+global _start
+
+_start:
+
+    mov     ebx, msg        ; moving address of string into ebx
+    mov      eax, ebx
+    
+nextchar:
+    cmp     byte [eax], 0   ; compare byte pointed to by EAX against zero (string delimiter)
+    jz      finished        ; if = 0, jump to finished
+    inc     eax             ; increment eax by 1
+    jmp     nextchar        ; jump back to nextchar and repeat
+
+finished:
+    sub     eax, ebx        ; eax has been 'forced' to be as long as our message 
+                            ; (+ original memory position). When we subtract ebx from it, 
+                            ; we get the exact size in bytes of our msg (+ null character)
+
+    mov     edx, eax        ; edx holds our count
+    mov     ecx, msg
+    mov     ebx, 1
+    mov     eax, 4
+    int     80h
+
+    mov     ebx, 0          ; We're behaving, so we return a nice value
+    mov     eax, 1
+    int     80h
--- Assembly/NASM/ASMtutor/readme.md
+++ Assembly/NASM/ASMtutor/readme.md
@@ -18,7 +18,9 @@
 
 **In C:**
 
-    ssize_t write(unsigned int fd, const char *buf, size_t count);
+    #include <unistd.h>
+
+    ssize_t write(int fd, const void buf[.count], size_t count);
 
 **Syscall table:**
 
@@ -32,4 +34,26 @@
     mov ebx, 1                       ; write to STDOUT
     mov eax, 4                       ; SYS_WRITE (kernel opcode 4)
     int 80h
+
+NOTES:
+
+- So, as we can see, the table is only showing us that ebx→fd, ecx→buf, edx→count (fd = 'file descriptor').
+- It is showing us the translation between the C API and the register-level ABI (Application Binary Interface).
+- size_t usually means number of bytes (not characters) not null terminated.
+- Man pages can tell us more (ex: 'man 2 write', in general 'man 2 syscallname')
+
+---
+
+Linux man pages sections:
+
+**man 1** → user commands (ls, cat, etc.)
+**man 2** → system calls (read, write, open, …)
+**man 3** → C library functions (printf, strcpy, …)
+**man 4** → device files
+**man 5** → file formats
+**man 7** → conventions, overview docs
+**man 8** → admin commands
+
+---
+
 
